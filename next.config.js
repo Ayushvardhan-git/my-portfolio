7efa9901eb35cebc999070238ae9c12e5eb5ed94
Ayush-vardhan <122ec0586@nitrkl.ac.in>
2025-02-15 02:03:41 +0530

    Fixed Next.js config for Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
